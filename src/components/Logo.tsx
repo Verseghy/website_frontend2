67dd2895016cd6ed5feb2923bf8d08bdf3bb15cf
Szepesi Tibor <sztibi023@gmail.com>
2023-09-24 13:19:28 +0200

    Fix logo link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,16 @@
 import { VoidComponent } from 'solid-js'
+import { A } from 'solid-start'
 import styles from './Logo.module.scss'
 import logo from '/assets/icons/logo.svg'
 
 const Logo: VoidComponent = () => {
   return (
-    <a href="/" class={styles.logo} tabindex="-1">
+    <A href="/" class={styles.logo} tabindex="-1">
       <img src={logo} alt="Verseghy Ferenc Gimnázium címere" />
       <p>
         Verseghy <span>WEB</span>
       </p>
-    </a>
+    </A>
   )
 }
 
