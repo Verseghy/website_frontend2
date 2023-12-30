c4d9193e19c772fc500b5e58b766a96c5215a3ae
Szepesi Tibor <sztibi023@gmail.com>
2023-12-30 02:19:25 +0100

    Add skip to content button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,26 @@ import Header from './Header'
 import styles from './Layout.module.scss'
 
 const Layout: ParentComponent = (props) => {
+  let ref: HTMLElement | undefined
+
+  const skipNav = () => {
+    console.log('skip nav', ref)
+    ref?.focus()
+  }
+
   return (
-    <div class={styles.layout}>
-      <Header />
-      <main class={styles.main}>{props.children}</main>
-      <Footer />
-    </div>
+    <>
+      <button onClick={skipNav} class={styles.skipToMain} aria-label="Navigáció átugrása">
+        <span>Navigáció átugrása</span>
+      </button>
+      <div class={styles.layout}>
+        <Header />
+        <main ref={ref} tabindex="-1" class={styles.main}>
+          {props.children}
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
 
