5a607325d27ffd039715db42362f1d6d3baad870
Szepesi Tibor <sztibi023@gmail.com>
2024-03-28 16:35:45 +0100

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig } from '@solidjs/start/config'
 
 export default defineConfig({
-  start: {
-    ssr: true,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: (source: string, file: string) => {
-          if (file.endsWith('import.scss') || file.endsWith('_breakpoints.scss')) {
-            return source
-          }
-          return `
+  ssr: true,
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: (source: string, file: string) => {
+            if (file.endsWith('import.scss') || file.endsWith('_breakpoints.scss')) {
+              return source
+            }
+            return `
             @use "./src/scss/import.scss" as *;
             ${source}
           `
+          },
         },
       },
-    },
-    modules: {
-      localsConvention: 'camelCase',
+      modules: {
+        localsConvention: 'camelCase',
+      },
     },
   },
 })
